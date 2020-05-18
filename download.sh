3aafce2dc34e9eebe5284ca79089cf5533b05c37
arunkumarv <gangaeyam@gmail.com>
2020-05-18 16:47:44 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/dashboard.html -O dashboard.html -P /usr/Apps/cov/views/mvd/chennai_dashboard
-wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/report.html -O report.html -P /usr/Apps/cov/views/mvd/chennai_dashboard
-wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/static/dashboard.js -O dashboard.js -P /usr/Apps/cov/public/scripts/libraries/chennai_dashboard/js
-wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/static/report.js  -O report.js -P /usr/Apps/cov/public/scripts/libraries/chennai_dashboard/js
+wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/dashboard.html  -P /usr/Apps/cov/views/mvd/chennai_dashboard -O dashboard.html
+wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/report.html -P /usr/Apps/cov/views/mvd/chennai_dashboard  -O report.html
+wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/static/dashboard.js  -P /usr/Apps/cov/public/scripts/libraries/chennai_dashboard/js -O dashboard.js
+wget https://raw.githubusercontent.com/arunkumarv/covid-survey/tvm/static/report.js   -P /usr/Apps/cov/public/scripts/libraries/chennai_dashboard/js -O report.js
